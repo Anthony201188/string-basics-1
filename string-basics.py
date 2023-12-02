7be0c90b9acc80607c55de1bfecf585134dba8f4
Anthony <anthony201188@hotmail.co.uk>
2023-12-02 21:22:07 +0100

    all tasks completed

---
+++ string-basics.py
@@ -0,0 +1,94 @@
+# task 1
+print()
+print("*******************")
+print("TASK-1")
+print()
+
+city = "london"
+print(city)
+
+
+print()
+print("*******************")
+
+print()
+print("*******************")
+print("TASK-2")
+print()
+
+#task 2
+
+city = "berlin"
+population = 364500
+
+
+print(city.upper(), ": ", population)
+
+print()
+print()
+print("*******************")
+
+print()
+print("*******************")
+print("TASK-3")
+print()
+
+# Task 3
+
+city = "london"
+population = 9000000
+
+print("city: ",city, city.isalpha())
+print("population: ",  population)
+print()
+print()
+print("*******************")
+
+print()
+print("*******************")
+print("TASK-4")
+print()
+#Task 4
+
+text = "Berlin is surrounded by the State of Brandenburg and the contiguous with potsdam, Brandenburg's capital."
+
+index = text.find("capital")
+print("capital: ",index)
+print()
+print()
+print()
+
+print()
+print("*******************")
+print("TASK-5")
+print()
+#Task 5
+
+text = "Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau"
+
+split_text = text.split()
+
+print(split_text)
+
+
+print()
+print()
+print()
+
+print()
+print("*******************")
+print("TASK-6")
+print()
+
+#Task 6
+
+text = "Berlin is surrouded by the State of Brandenburg and contigous with Potsdam, Brandemburg's capital."
+
+print(text.replace("capital", "capital of Germnay"))
+
+print()
+print()
+print()
+
+print()
+print("*******************")
